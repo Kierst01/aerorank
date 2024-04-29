24bce8db0cc696836f39a96e95d6de55cf3f5258
Jon Gilberg <j.gilberg@outlook.com>
2024-04-28 19:24:51 -0700

    Initial openSky api tests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
+api_url = "https://opensky-network.org/api/states/all?lamin=48.8135&lomin=-123.7015&lamax=49.4306&lomax=-122.2843"
 response = requests.get(api_url)
 response.json()
+print(response.json())
