427572d4c3c8274056e74729d931724ec127b076
Kiersten Gilberg <kierstengilberg@gmail.com>
2024-05-04 16:46:00 -0700

    started function for converting back to lat lon

--- functions.py
+++ functions.py
@@ -19,3 +19,24 @@ def coord_to_cartesian(latitude, longitude):
     y_dist = 111.320 * math.cos(latitude * (2 * math.pi / 360)) * longitude
 
     return (x_dist, y_dist)
+
+
+# def cartesian_to_coord(x_dist, y_dist):
+#     """This function will approximately convert a latitude and longitude value to a point on the cartesian coordinate system
+    
+#     params:
+#     x_dist: float
+#         The approximate distance from the origin in cartesian coordinates
+#     y_dist: float
+#         The approximate distance form the origin in cartesian coordinates
+
+#     returns:
+#     latitude: float
+#         The latitude value
+#     longitude: float 
+#         The longitude value
+#     """
+#     latitude = x_dist / 110.574
+#     longitude = 
+
+#     return (latitude, longitude)
