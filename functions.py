c3131ed90540a4491ebbe48a59f621c6fa4c8855
Jon Gilberg <j.gilberg@outlook.com>
2024-05-04 17:13:58 -0700

    Completed cartesian_to_coord function

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import math
 
 def coord_to_cartesian(latitude, longitude):
-    """This function will approximately convert a latitude and longitude value to a point on the cartesian coordinate system
+    """This function will approximately convert a latitude and longitude value to a point on a cartesian coordinate system in units of kilometers
     
     params:
     latitude: float
@@ -11,9 +11,9 @@ def coord_to_cartesian(latitude, longitude):
 
     returns:
     x_dist: float
-        The approximate distance from the origin in cartesian coordinates
+        The approximate distance in kilometers from the origin in cartesian coordinates
     y_dist: float
-        The approximate distance form the origin in cartesian coordinates
+        The approximate distance in kilometers from the origin in cartesian coordinates
     """
     x_dist = 110.574 * latitude
     y_dist = 111.320 * math.cos(latitude * (2 * math.pi / 360)) * longitude
@@ -21,22 +21,22 @@ def coord_to_cartesian(latitude, longitude):
     return (x_dist, y_dist)
 
 
-# def cartesian_to_coord(x_dist, y_dist):
-#     """This function will approximately convert a latitude and longitude value to a point on the cartesian coordinate system
+def cartesian_to_coord(x_dist, y_dist):
+    """This function will approximately convert from a cartesian coordinate system in units of kilometers to latitude and longitude
     
-#     params:
-#     x_dist: float
-#         The approximate distance from the origin in cartesian coordinates
-#     y_dist: float
-#         The approximate distance form the origin in cartesian coordinates
+    params:
+    x_dist: float
+        The approximate distance from the origin in cartesian coordinates
+    y_dist: float
+        The approximate distance form the origin in cartesian coordinates
 
-#     returns:
-#     latitude: float
-#         The latitude value
-#     longitude: float 
-#         The longitude value
-#     """
-#     latitude = x_dist / 110.574
-#     longitude = 
+    returns:
+    latitude: float
+        The latitude value
+    longitude: float 
+        The longitude value
+    """
+    latitude = x_dist / 110.574
+    longitude = y_dist / 111.320 / math.cos(latitude * (2 * math.pi / 360))
 
-#     return (latitude, longitude)
+    return (latitude, longitude)
